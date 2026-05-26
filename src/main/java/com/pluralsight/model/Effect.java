1309031cc8450f8bdff924f6a720734503f5e374
ramenmac <kevinnguyen4200@gmail.com>
2026-05-26 11:18:50 -0500

    Changing potion logic, create OptionalEffect class

--- src/main/java/com/pluralsight/model/Effect.java
+++
@@ -1,6 +0,0 @@
-package com.pluralsight.model;
-
-public class Effect {
-    private String name;
-
-}
---
+++ src/main/java/com/pluralsight/model/OptionalEffect.java
@@ -0,0 +1,21 @@
+package com.pluralsight.model;
+
+public class OptionalEffect {
+    private String name, type;
+    private boolean premium;
+
+    OptionalEffect(String name, String type, boolean premium){
+        this.name = name;
+        this.type = type;
+        this.premium = premium;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public boolean isPremium() { return premium; }
+    public void setPremium(boolean premium) { this.premium = premium; }
+}
