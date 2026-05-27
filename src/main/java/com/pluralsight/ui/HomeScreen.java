d585ec4ffb52f3924016000aa7594753687dab83
ramenmac <kevinnguyen4200@gmail.com>
2026-05-27 03:09:53 -0500

    Clean up home screen and add comments

--- src/main/java/com/pluralsight/ui/HomeScreen.java
+++ src/main/java/com/pluralsight/ui/HomeScreen.java
@@ -6,16 +6,22 @@ public class HomeScreen {
     static Scanner input = new Scanner(System.in);
 
     public void run(){
+
         boolean isRunning = true;
 
-
+        // Cleans console and display home screen
         clearConsole();
+        displayScreen();
+
+
         while(isRunning){
-            displayScreen();
             isRunning = processSelection(promptSelection());
         }
     }
+
     public static void displayScreen(){
+
+        // Menu with our villager trader
         System.out.println("""
                 ===================================================================
                                                WELCOME TO BLOCKY BREWS
@@ -35,18 +41,32 @@ public class HomeScreen {
     }
 
     public String promptSelection(){
+
+        // Prompt the user for their choice and returns the trimmed input
         System.out.println("Enter in your choice");
         return input.nextLine().trim();
     }
+
     public boolean processSelection(String userChoice){
+
+        // Switch case based off user input
         switch(userChoice){
+
             case "1":
                 OrderScreen orderScreen = new OrderScreen();
                 orderScreen.run();
+
+                // Clears and displays home again when return from order screen
+                clearConsole();
+                displayScreen();
                 return true;
+
+                // Returns false to end program / while loop
             case "0":
                 return false;
+
             default:
+                // *Angry villager noise*
                 System.out.println("HRGHHHHHH! (Enter a valid choice)");
                 return true;
         }
