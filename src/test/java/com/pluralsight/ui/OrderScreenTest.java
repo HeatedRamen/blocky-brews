1f6efafe846ecfc8bf34fc82fae0dc00bdf9163e
ramenmac <kevinnguyen4200@gmail.com>
2026-05-27 23:35:00 -0500

    Prep test cases

---
+++ src/test/java/com/pluralsight/ui/OrderScreenTest.java
@@ -0,0 +1,27 @@
+package com.pluralsight.ui;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+class OrderScreenTest {
+
+    @Test
+    void addTrade() {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @Test
+    void processTradeSelection() {
+    }
+
+    @Test
+    void addBasePotion() {
+    }
+
+    @Test
+    void processBasePotionSelection() {
+    }
+}
