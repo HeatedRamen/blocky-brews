3f704dd8def1f8279ab8da53d3536d3353f2aa39
ramenmac <kevinnguyen4200@gmail.com>
2026-05-28 17:05:35 -0500

    Add potion feature

--- src/main/java/com/pluralsight/ui/OrderScreen.java
+++ src/main/java/com/pluralsight/ui/OrderScreen.java
@@ -4,11 +4,6 @@ import com.pluralsight.model.*;
 import com.pluralsight.util.InputValidation;
 import com.pluralsight.data.ReceiptWriter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-
 public class OrderScreen {
 
     private Order userOrder = new Order();
@@ -56,25 +51,212 @@ public class OrderScreen {
         }
     }
 
-    public void makePotion() {
-        promptPotion();
+    public void makePotion(){
+
+        Potion potion = new Potion();
+
+        clearConsole();
+        promptMainPotionEffect();
+        potion.setMainEffect(processMainEffect(InputValidation.getValidInput
+                                            (InputValidation.options(1, 6))));
+
+        clearConsole();
+        promptPotionSize();
+        potion.setSize(processPotionSize(InputValidation.getValidInput
+                                   (InputValidation.options(1, 3))));
+
+        clearConsole();
+        promptPotionForm();
+        processPotionForm(InputValidation.getValidInput
+                        (InputValidation.options(2)), potion);
+
+        clearConsole();
+        promptEnhancement();
+        processEnhancement(InputValidation.getValidInput
+                    (InputValidation.options(3)), potion);
+
+        clearConsole();
+        promptAdditionalEffect();
+        processAdditionalEffect(InputValidation.getValidInput
+                    (InputValidation.options(5)), potion);
+
+        clearConsole();
+        promptVisualEffect();
+        processVisualEffect(InputValidation.getValidInput
+                (InputValidation.options(5)), potion);
+
+        clearConsole();
+        promptPotionHelper();
+        processPotionHelper(InputValidation.getValidInput
+                    (InputValidation.options(2)), potion);
+
+        userOrder.addItem(potion);
     }
 
-    public void promptPotion(){
-        System.out.println(MenuStrings.MAIN_EFFECT_MENU());
+    public String processMainEffect(String userChoice){
+        switch (userChoice){
 
-        System.out.println(MenuStrings.POTION_SIZE());
+            case "1":
+                return "Strength";
 
-        System.out.println(MenuStrings.POTION_FORM_MENU());
+            case "2":
+                return "Swiftness";
 
-        System.out.println(MenuStrings.ADDITIONAL_EFFECT_MENU());
+            case "3":
+                return "Healing";
 
-        System.out.println(MenuStrings.POTION_ENHANCEMENT());
+            case "4":
+                return "Weakness";
 
-        System.out.println(MenuStrings.VISUAL_EFFECT_MENU());
+            case "5":
+                return "Slowness";
 
+            case "6":
+                return "Poison";
 
-        System.out.println(MenuStrings.POTION_HELPER_MENU());
+            default:
+                return "";
+        }
+    }
+
+    public Potion.PotionSize processPotionSize(String userChoice){
+        switch (userChoice){
+            case "1":
+                return Potion.PotionSize.SMALL;
+            case "2":
+                return Potion.PotionSize.MEDIUM;
+            case "3":
+                return Potion.PotionSize.LARGE;
+            default:
+                return null;
+        }
+    }
+
+    public void processPotionForm(String userChoice , Potion potion){
+        switch (userChoice){
+
+            case "1":
+                potion.addOptionalEffect( new OptionalEffect("Throwable", "Form", false));
+                break;
+
+            case "2":
+                potion.addOptionalEffect( new OptionalEffect("Lingering", "Form", false));
+                break;
+
+            case "0":
+                potion.addOptionalEffect( new OptionalEffect("Consumable", "Form", false));
+                break;
+        }
+    }
+
+    public void processEnhancement(String userChoice, Potion potion) {
+        switch (userChoice) {
+            case "1":
+                potion.addOptionalEffect( new OptionalEffect("Stronger", "Enhancement", true));
+                break;
+
+            case "2":
+                potion.addOptionalEffect( new OptionalEffect("Longer", "Enhancement", true));
+                break;
+
+            case "3":
+                potion.addOptionalEffect( new OptionalEffect("Longer", "Enhancement", true));
+                potion.addOptionalEffect( new OptionalEffect("Stronger", "Enhancement", true));
+                break;
+
+            case "0":
+                return;
+        }
+    }
+
+    public void processAdditionalEffect(String userChoice, Potion potion){
+
+        int count = 0;
+        while (!userChoice.equals("0") && count < 2){
+
+            switch (userChoice){
+
+                case "1":
+                    potion.addOptionalEffect( new OptionalEffect("Water Breathing", "Additional Effect", true));
+                    count++;
+                    break;
+
+                case "2":
+                    potion.addOptionalEffect( new OptionalEffect("Fire Resistance", "Additional Effect", true));
+                    count++;
+                    break;
+
+                case "3":
+                    potion.addOptionalEffect( new OptionalEffect("Night Vision", "Additional Effect", true));
+                    count++;
+                    break;
+
+                case "4":
+                    potion.addOptionalEffect( new OptionalEffect("Invisibility", "Additional Effect", true));
+                    count++;
+                    break;
+
+                case "5":
+                    potion.addOptionalEffect( new OptionalEffect("Slow Falling", "Additional Effect", true));
+                    count++;
+                    break;
+            }
+
+            userChoice = InputValidation.getValidInput
+                    (InputValidation.options(5));
+        }
+    }
+
+    public void processVisualEffect(String userChoice, Potion potion){
+
+        while (!userChoice.equals("0")){
+
+            switch (userChoice){
+
+                case "1":
+                    potion.addOptionalEffect( new OptionalEffect("Glows", "Visual", false));
+                    break;
+
+                case "2":
+                    potion.addOptionalEffect( new OptionalEffect("Swirly", "Visual", false));
+                    break;
+
+                case "3":
+                    potion.addOptionalEffect( new OptionalEffect("Diamond Sparkle", "Visual", false));
+                    break;
+
+                case "4":
+                    potion.addOptionalEffect( new OptionalEffect("Gold Flakes", "Visual", false));
+                    break;
+
+                case "5":
+                    potion.addOptionalEffect( new OptionalEffect("Emerald Color", "Visual", false));
+                    break;
+            }
+
+            userChoice = InputValidation.getValidInput
+                        (InputValidation.options(5));
+        }
+    }
+
+    public void processPotionHelper(String userChoice, Potion potion){
+
+        while (!userChoice.equals("0")){
+
+            switch (userChoice){
+
+                case "1":
+                    potion.addOptionalEffect( new OptionalEffect("Rotten Flesh", "Potion Helper", false));
+                    break;
+
+                    case "2":
+                    potion.addOptionalEffect( new OptionalEffect("Nether Warts", "Potion Helper", false));
+                    break;
+            }
+
+            userChoice = InputValidation.getValidInput
+                    (InputValidation.options(2));
+        }
     }
 
     public void addBasePotion(){
@@ -199,6 +381,13 @@ public class OrderScreen {
 
    // Menu output
     public static void displayScreen() { System.out.println(MenuStrings.ORDER_MENU()); }
+    public static void promptMainPotionEffect() { System.out.println(MenuStrings.MAIN_EFFECT_MENU()); }
+    public static void promptPotionSize() { System.out.println(MenuStrings.POTION_SIZE()); }
+    public static void promptPotionForm() { System.out.println(MenuStrings.POTION_FORM_MENU());}
+    public static void promptAdditionalEffect(){ System.out.println(MenuStrings.ADDITIONAL_EFFECT_MENU()); }
+    public static void promptEnhancement(){ System.out.println(MenuStrings.POTION_ENHANCEMENT()); }
+    public static void promptVisualEffect(){ System.out.println(MenuStrings.VISUAL_EFFECT_MENU()); }
+    public static void promptPotionHelper(){ System.out.println(MenuStrings.POTION_HELPER_MENU()); }
     public static void promptBasePotionName() { System.out.println(MenuStrings.BASE_POTION_MENU()); }
     public static void promptBasePotionSize(){ System.out.println(MenuStrings.BASE_POTION_SIZE_MENU()); }
     public static void promptTrade(){ System.out.println(MenuStrings.TRADE_MENU()); }
