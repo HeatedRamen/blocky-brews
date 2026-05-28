4b08b7a47ca53fc1afe2425a333c39b787f9c4f0
ramenmac <kevinnguyen4200@gmail.com>
2026-05-27 23:34:27 -0500

    Prep prompts for potion making

--- src/main/java/com/pluralsight/ui/OrderScreen.java
+++ src/main/java/com/pluralsight/ui/OrderScreen.java
@@ -53,7 +53,87 @@ public class OrderScreen {
 
     public void promptPotion(){
         System.out.println("""
-                1) """);
+                ===================================================================
+                                       Main Potion effect
+                ===================================================================
+                
+                                    1) Strength
+                                    2) Swiftness
+                                    3) Healing
+                                    4) Weakness
+                                    5) Slowness
+                                    6) Poison
+                
+                ===================================================================""");
+
+        System.out.println("""
+                ===================================================================
+                                           Potion Size
+                ===================================================================
+                
+                                    1) Small
+                                    2) Medium
+                                    3) Large
+                
+                ===================================================================""");
+
+        System.out.println("""
+                ===================================================================
+                                    Additional Premium Attributes
+                ===================================================================
+                
+                                    1) Water Breathing
+                                    2) Fire Breathing
+                                    3) Night Vision
+                                    4) Invisibility
+                                    5) Slow Falling
+                
+                ===================================================================""");
+
+
+        System.out.println("""
+                ===================================================================
+                                       Potion Enhancements 
+                ===================================================================
+                
+                                    1) Stronger effect
+                                    2) Longer effect
+                                   
+                
+                ===================================================================""");
+
+        System.out.println("""
+                ===================================================================
+                                           Visuals Effects
+                ===================================================================
+                
+                                    1) Glows
+                                    2) Swirly
+                                    3) Diamond sparkle
+                                    4) 
+                
+                ===================================================================""");
+
+        System.out.println("""
+                ===================================================================
+                                           Change Potion Form
+                ===================================================================
+                
+                                    (Default: Consumable)
+                                    1) Throwable
+                                    2) Lingering
+                
+                ===================================================================""");
+
+        System.out.println("""
+                ===================================================================
+                                                Potion Helpers
+                ===================================================================
+                
+                                    1) Rotten Flesh
+                                    2) Nether warts
+                
+                ===================================================================""");
     }
 
     public void addBasePotion(){
@@ -61,6 +141,13 @@ public class OrderScreen {
         clearConsole();
         promptBasePotionName();
         String potionName = promptSelection();
+
+        // Check if they canceled
+        if(potionName.equalsIgnoreCase("0")){
+            VillagerExpression.angry();
+            return;
+        }
+
         promptBasePotionSize();
         processBasePotionSelection(potionName, promptSelection());
 
@@ -72,22 +159,25 @@ public class OrderScreen {
                 ===================================================================
                                             Base Potion
                 ===================================================================
-                
+
                                             1) Mundane
                                             2) Awkward
                                             3) Thick
-                
+                                            0) Cancel
+
                 ===================================================================""");
     }
+
     public void promptBasePotionSize(){
         System.out.println("""
                 ===================================================================
-                                        Tradable Items
+                                               Sizes
                 ===================================================================
                 
-                                        1) Small        (2 Emeralds)
-                                        2) Medium       (3 Emeralds)
-                                        3) Large        (4 Emeralds)
+                                       1) Small        (2 Emeralds)
+                                       2) Medium       (3 Emeralds)
+                                       3) Large        (4 Emeralds)
+                                       0) Cancel
                 
                 ===================================================================""");
     }
@@ -108,6 +198,7 @@ public class OrderScreen {
             case "1" -> userOrder.addItem(new PotionBase(potionName, PotionBase.Size.SMALL));
             case "2" -> userOrder.addItem(new PotionBase(potionName, PotionBase.Size.MEDIUM));
             case "3" -> userOrder.addItem(new PotionBase(potionName, PotionBase.Size.LARGE));
+            case "0" -> VillagerExpression.angry();
             default -> System.out.println("HRGHHHHHH! (Enter a valid choice)");
         }
     }
@@ -121,12 +212,12 @@ public class OrderScreen {
     public void promptTrade(){
         System.out.println("""
                 ===================================================================
-                                        Tradable Items
+                                           Tradable Items
                 ===================================================================
                 
-                                        1) Golden Carrot (5 Emeralds)
-                                        2) XP bottle     (10 Emeralds)
-                                        3) Go back
+                                    1) Golden Carrot (5 Emeralds)
+                                    2) XP bottle     (10 Emeralds)
+                                    3) Go back
                 
                 ===================================================================""");
     }
@@ -150,6 +241,7 @@ public class OrderScreen {
     }
 
     public void checkOut() {
+        clearConsole();
         userOrder.displayOrder();
     }
 
